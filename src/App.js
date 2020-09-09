16cf48a4e15423b63ae5f1e6e96104b165262277
asharma984 <aditya.sharma984@gmail.com>
2020-09-08 23:33:37 -0400

    Added components folder and alan key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import alanBtn from '@alan-ai/alan-sdk-web'
 
+const alanKey='cb6d72d7bd7841c473153a837c80fba82e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
+   const [newsArticles, setNewsArticles] =useState([]);
+    useEffect(() => {
+       alanBtn({
+         key:alanKey,
+         onCommand:({command, articles}) => {
+             if(command==='newHeadlines')
+             {
+                 setNewsArticles(articles);
+             }
+         }
+       })
+    }, [] )
+
     return(
        <div>
            <h1>News Application</h1>
